74bbca666a8ed20d9bbdf3e44d4c1ee42000f2af
Koh Mingyang <kohmingyang@outlook.com>
2024-12-06 11:06:22 +0800

    docs: added comments + read db password natively in dev

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
 	"github.com/pressly/goose/v3"
 	"github.com/rs/zerolog"
 )
@@ -25,32 +26,42 @@ func main() {
 	// Retrieve DB password from secrets
 	db_pw, err := os.ReadFile("/run/secrets/db-password")
 	if err != nil {
-		logger.Panic().Msg("Error reading secret")
+		logger.Error().Msg("Error reading secret")
+
+		// Try reading from .env
+		if err := godotenv.Load(".env.dev"); err != nil {
+			logger.Panic().Err(err).Msg("Error reading from .env")
+		}
+		db_pw = []byte(os.Getenv("POSTGRES_PW"))
 	}
 
 	// Retrieve env variables
 	postgres_db, exists := os.LookupEnv("POSTGRES_DB")
 	if !exists {
+		// Defaults to DEV_1
 		logger.Error().Msg("Error retrieving postgres database name, default name set.")
 		postgres_db = "DEV_1"
 	}
 
+	// Create connection pool to db
 	connection_string := fmt.Sprintf("postgres://postgres:%s@db:5432/%s?sslmode=disable", string(db_pw), postgres_db)
 	dbpool, err := pgxpool.New(context.Background(), connection_string)
+	if err != nil {
+		logger.Panic().Err(err).Msg("Error creating connection pool")
+	}
+	defer dbpool.Close()
 
+	// Initialize Goose and perform migrations
 	if err := goose.SetDialect("postgres"); err != nil {
-		panic(err)
+		logger.Panic().Err(err)
 	}
 
 	db := stdlib.OpenDBFromPool(dbpool)
 	if err := goose.Up(db, "migrations"); err != nil {
-		panic(err)
+		logger.Panic().Err(err)
 	}
 	if err := db.Close(); err != nil {
-		panic(err)
-	}
-	if err != nil {
-		logger.Panic().Msg("Error connecting to DB")
+		logger.Panic().Err(err)
 	}
 
 	r := gin.Default()
