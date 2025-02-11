dd08bf25901e226d014955585cf9766e58718c30
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2025-02-11 13:26:32 +0800

    feat(middlewares): add AuthGuard middleware for token verification

--- internal/api/middlewares/auth.go
+++ internal/api/middlewares/auth.go
@@ -1,14 +1,41 @@
-package auth
+package middlewares
 
 import (
 	"github.com/gin-gonic/gin"
 	utils "github.com/ntu-onemdp/onemdp-backend/internal/utils"
 )
 
+// Verification middleware for non-public routes. Reject if invalid auth token
+func AuthGuard() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		utils.Logger.Trace().Msg("AuthGuard triggered")
+
+		// Retrieve jwt token from auth header
+		tokenString := c.Request.Header.Get("Authorization")
+
+		// Remove "Bearer " prefix if included
+		if len(tokenString) > 7 && tokenString[:7] == "Bearer " {
+			tokenString = tokenString[7:]
+		}
+
+		claim, err := utils.ParseJwt(tokenString)
+
+		if err != nil {
+			c.JSON(401, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		utils.Logger.Trace().Msg(claim.Username)
+		utils.Logger.Trace().Msg(claim.Role)
+		c.Next()
+	}
+}
+
 // Verification middleware for admin. Reject if not admin.
 func AdminGuard() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		utils.Logger.Trace().Msg("Guard triggered")
+		utils.Logger.Trace().Msg("AdminGuard triggered")
 
 		// Retrieve jwt token from auth header
 		tokenString := c.Request.Header.Get("Authorization")
