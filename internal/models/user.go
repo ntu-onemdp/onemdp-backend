a68385d8cd9e186ea10e0704433bda2902ffd4a2
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2025-02-15 16:01:40 +0800

    feat(user): initial commit for InsertManyUsers function

--- internal/models/user.go
+++ internal/models/user.go
@@ -10,3 +10,18 @@ type User struct {
 	ProfilePhoto    string `json:"profile_photo"`
 	Status          string `json:"status"`
 }
+
+// For dev use only
+// Initialize an empty user for testing
+func CreateUser(username string, name string) *User {
+	return &User{
+		Username:        username,
+		Name:            name,
+		DateCreated:     "",
+		DateRemoved:     "",
+		Semester:        1,
+		PasswordChanged: false,
+		ProfilePhoto:    "",
+		Status:          "",
+	}
+}
--- internal/repositories/users_repository.go
+++ internal/repositories/users_repository.go
@@ -16,7 +16,7 @@ type UsersRepository struct {
 // Retrieve user's status based on the username.
 // Throws error if username cannot be found
 func (r *UsersRepository) GetStatusByUsername(username string) (string, error) {
-	query := "SELECT status FROM users WHERE username=$1"
+	query := "SELECT status FROM users WHERE username=$1;"
 
 	var status string
 	err := r.Db.QueryRow(context.Background(), query, username).Scan(&status)
@@ -32,7 +32,7 @@ func (r *UsersRepository) GetStatusByUsername(username string) (string, error) {
 // This function *checks* if user is active before returning. If the user's status is not 'active',
 // an error is return instead.
 func (r *UsersRepository) GetUserByUsername(username string) (*models.User, error) {
-	query := "SELECT * FROM users WHERE username=$1 AND status = 'active'"
+	query := "SELECT * FROM users WHERE username=$1 AND status = 'active';"
 
 	row, _ := r.Db.Query(context.Background(), query, username)
 	user, err := pgx.RowToStructByName[models.User](row)
@@ -44,3 +44,57 @@ func (r *UsersRepository) GetUserByUsername(username string) (*models.User, erro
 
 	return &user, nil
 }
+
+// This method does not work for now. Explore in the future when there is time.
+// func (r *UsersRepository) InsertManyUsers(users []models.User) error {
+
+// 	// Open a transaction
+// 	tx, err := r.Db.Begin(context.Background())
+// 	if err != nil {
+// 		utils.Logger.Error().Err(err)
+// 		return err
+// 	}
+// 	defer tx.Rollback(context.Background())
+
+// 	utils.Logger.Trace().Msg("fn called")
+// 	// Create temporary staging table
+// 	STAGING_USERS := "staging_users" // temp staging table name
+// 	_, err = r.Db.Exec(context.Background(), "CREATE TEMP TABLE staging_users (username TEXT PRIMARY KEY,name TEXT);")
+// 	if err != nil {
+// 		utils.Logger.Error().Err(err)
+// 		return err
+// 	}
+
+// 	// Prepare copy operation
+// 	copyCount, err := tx.CopyFrom(context.Background(), pgx.Identifier{STAGING_USERS}, []string{"username, name"}, pgx.CopyFromSlice(len(users), func(i int) ([]any, error) {
+// 		return []any{users[i].Username, users[i].Name}, nil
+// 	}))
+
+// 	if err != nil {
+// 		utils.Logger.Error().Err(err)
+// 		return err
+// 	}
+
+// 	utils.Logger.Debug().Msg("Users copied to staging table")
+
+// 	// Move data to final table
+// 	_, err = tx.Exec(context.Background(), `
+// 	INSERT INTO users (username, name)
+// 	SELECT s.username, s.name
+// 	FROM staging_users s
+// 	WHERE NOT EXISTS (
+// 		SELECT 1 FROM users u WHERE u.username = s.username
+// 		)
+// 		ON CONFLICT (username) DO UPDATE
+// 		SET name = EXCLUDED.name
+// 		`)
+
+// 	if err != nil {
+// 		utils.Logger.Error().Err(err)
+// 		return err
+// 	}
+
+// 	utils.Logger.Trace().Msg(strconv.FormatInt(copyCount, 10))
+
+// 	return tx.Commit(context.Background())
+// }
