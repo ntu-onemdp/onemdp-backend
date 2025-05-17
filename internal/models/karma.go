943cd0441627481a11b1310c577fe1d4e707e11d
Koh Mingyang <kohmingyang@outlook.com>
2025-05-17 22:28:23 +0800

    feat(karma): added karma attribute to users

---
+++ internal/models/karma.go
@@ -0,0 +1,9 @@
+package models
+
+// This file contains the points awarded to a user for various actions under the karma points system.
+
+const (
+	CREATE_THREAD_PTS = 10
+	POST_REPLY_PTS    = 5
+	LIKE_PTS          = 1
+)
