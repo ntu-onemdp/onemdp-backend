98eb30458a0420884b113f867db30a77b5fa3fe2
Koh Mingyang <kohmingyang@outlook.com>
2025-06-27 21:03:38 +0800

    feat(comments): added comment factory and method to generate dbcomment. fixed time created and last edited field from string to time

--- internal/models/comment.go
+++ internal/models/comment.go
@@ -1,5 +1,13 @@
 package models
 
+import (
+	"time"
+
+	gonanoid "github.com/matoous/go-nanoid/v2"
+	"github.com/microcosm-cc/bluemonday"
+	constants "github.com/ntu-onemdp/onemdp-backend/config"
+)
+
 // Comment models how a comment is represented on the API.
 type Comment struct {
 	DbComment
@@ -11,14 +19,14 @@ type Comment struct {
 
 // DbComment models how a comment is stored in the database
 type DbComment struct {
-	CommentID   string `json:"comment_id" db:"comment_id"`
-	AuthorUID   string `json:"author_uid" db:"author" binding:"required"`
-	ArticleID   string `json:"article_id" db:"article_id" `
-	Content     string `json:"content" db:"content" binding:"required"`
-	TimeCreated string `json:"time_created" db:"time_created"`
-	LastEdited  string `json:"last_edited" db:"last_edited"`
-	Flagged     bool   `json:"flagged" db:"flagged"`
-	IsAvailable bool   `json:"is_available" db:"is_available"`
+	CommentID   string    `json:"comment_id" db:"comment_id"`
+	AuthorUID   string    `json:"author_uid" db:"author" binding:"required"`
+	ArticleID   string    `json:"article_id" db:"article_id" `
+	Content     string    `json:"content" db:"content" binding:"required"`
+	TimeCreated time.Time `json:"time_created" db:"time_created"`
+	LastEdited  time.Time `json:"last_edited" db:"last_edited"`
+	Flagged     bool      `json:"flagged" db:"flagged"`
+	IsAvailable bool      `json:"is_available" db:"is_available"`
 }
 
 type CommentFactory struct {
@@ -29,6 +37,19 @@ func NewCommentFactory() *CommentFactory {
 	return &CommentFactory{}
 }
 
-func (f *CommentFactory) New(authorUID string, articleID string) {
+func (f *CommentFactory) New(authorUID string, articleID string, content string) *DbComment {
+	// Sanitize content to prevent XSS attacks
+	policy := bluemonday.UGCPolicy()
+	content = policy.Sanitize(content)
 
+	return &DbComment{
+		CommentID:   "c" + gonanoid.Must(constants.CONTENT_ID_LENGTH),
+		AuthorUID:   authorUID,
+		ArticleID:   articleID,
+		Content:     content,
+		TimeCreated: time.Now(),
+		LastEdited:  time.Now(),
+		Flagged:     false,
+		IsAvailable: true,
+	}
 }
