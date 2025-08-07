62eef294e0f1f3b39e736b22fa260884578a2505
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2025-08-07 14:57:55 +0800

    fix(migrations)!: fixed initialization for file table

--- migrations/20250806074846_init_files.sql
+++ migrations/20250806074846_init_files.sql
@@ -1,25 +1,19 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE public.files
-(
+CREATE TABLE IF NOT EXISTS public.files (
     file_id text NOT NULL,
     author text NOT NULL,
     filename text NOT NULL,
     gcs_filename text NOT NULL,
-    status text NOT NULL DEFAULT 'available',
+    status text NOT NULL DEFAULT 'available'::text,
     time_created timestamp with time zone NOT NULL DEFAULT now(),
     time_deleted timestamp with time zone,
     deleted_by text,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_files_author_users_uid FOREIGN KEY (author)
-        REFERENCES public.users (uid) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE NO ACTION
-        NOT VALID
+    file_group text,
+    PRIMARY KEY (file_id),
+    CONSTRAINT fk_files_author_users_uid FOREIGN KEY (author) REFERENCES public.users (uid) MATCH SIMPLE ON UPDATE CASCADE ON DELETE NO ACTION NOT VALID
 );
-
 -- +goose StatementEnd
-
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS public.files;
